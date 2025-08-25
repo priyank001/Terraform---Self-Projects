025400c78cd5e8f96735b3d84cbba5efba2c29c3
Priyank Namdev <priyank.namdev@gmail.com>
2025-08-25 12:50:59 +0100

    Remove access hard-codes

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,14 +1,17 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "accesskey"
-  secret_key = "secretkey"
+  # profile    = "zeal-east-1-profile"
+  profile    = "terraformUser"
 }
 
 resource "aws_instance" "terraform-ec2" {
   ami           = "ami-00ca32bbc84273381" # Amazon Linux 2 AMI US East-1 North Virginia
   instance_type = "t3.micro"
 
+  # Specify the subnet_id for the instance
+  # subnet_id     = "subnet-05495b8009e96720c" # TODO: Replace with a valid subnet ID from your default VPC
+
   tags = {
-    Name = "MyFirstTerraformEC2"
+    Name = "terraform-ec2"
   }
 }
