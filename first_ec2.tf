700d3db9eb43405ae67250e90138219a70e55d37
Priyank Namdev <priyank.namdev@gmail.com>
2025-08-25 10:41:24 +0100

    Add Terraform EC2 instaance

--- first_ec2.tf
+++ first_ec2.tf
@@ -3,3 +3,12 @@ provider "aws" {
   access_key = "accesskey"
   secret_key = "secretkey"
 }
+
+resource "aws_instance" "terraform-ec2" {
+  ami           = "ami-00ca32bbc84273381" # Amazon Linux 2 AMI US East-1 North Virginia
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "MyFirstTerraformEC2"
+  }
+}
